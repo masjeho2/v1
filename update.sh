3908694edef4b64d64ab57a49de995f558f9d4ec
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-05-22 13:29:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,124 @@
 #!/bin/bash
+cd /usr/bin
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Main Menu${NC}"
+wget -q -O menu "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/menu.sh"
+wget -q -O vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/vmess.sh"
+wget -q -O vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/vless.sh"
+wget -q -O trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/trojan.sh"
+wget -q -O socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/socks.sh"
+wget -q -O allsing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/menu/allsing-box.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vmess${NC}"
+wget -q -O add-vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vmess/add-vmess.sh"
+wget -q -O del-vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vmess/del-vmess.sh"
+wget -q -O extend-vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vmess/extend-vmess.sh"
+wget -q -O trialvmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vmess/trialvmess.sh"
+wget -q -O cek-vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vmess/cek-vmess.sh" 
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vless${NC}"
+wget -q -O add-vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vless/add-vless.sh"
+wget -q -O del-vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vless/del-vless.sh"
+wget -q -O extend-vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vless/extend-vless.sh"
+wget -q -O trialvless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vless/trialvless.sh"
+wget -q -O cek-vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/vless/cek-vless.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Trojan${NC}"
+wget -q -O add-trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/trojan/add-trojan.sh"
+wget -q -O del-trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/trojan/del-trojan.sh"
+wget -q -O extend-trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/trojan/extend-trojan.sh"
+wget -q -O trialtrojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/trojan/trialtrojan.sh"
+wget -q -O cek-trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/trojan/cek-trojan.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Socks5${NC}"
+wget -q -O add-socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/socks/add-socks.sh"
+wget -q -O del-socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/socks/del-socks.sh"
+wget -q -O extend-socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/socks/extend-socks.sh"
+wget -q -O trialsocks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/socks/trialsocks.sh"
+wget -q -O cek-socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/socks/cek-socks.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu All sing-box${NC}"
+wget -q -O add-sing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/allsing-box/add-sing-box.sh"
+wget -q -O del-sing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/allsing-box/del-sing-box.sh"
+wget -q -O extend-sing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/allsing-box/extend-sing-box.sh"
+wget -q -O trialsing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/allsing-box/trialsing-box.sh"
+wget -q -O cek-sing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/allsing-box/cek-sing-box.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Log${NC}"
+wget -q -O log-create "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-create.sh"
+wget -q -O log-vmess "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-vmess.sh"
+wget -q -O log-vless "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-vless.sh"
+wget -q -O log-trojan "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-trojan.sh"
+wget -q -O log-socks "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-socks.sh"
+wget -q -O log-allsing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/log/log-allsing-box.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Other Menu${NC}"
+wget -q -O xp "https://raw.githubusercontent.com/masjeho2/v1/sing-box/other/xp.sh"
+wget -q -O dns "https://raw.githubusercontent.com/masjeho2/v1/sing-box/other/dns.sh"
+wget -q -O certsing-box "https://raw.githubusercontent.com/masjeho2/v1/sing-box/other/certsing-box.sh"
+wget -q -O about "https://raw.githubusercontent.com/masjeho2/v1/sing-box/other/about.sh"
+wget -q -O clear-log "https://raw.githubusercontent.com/masjeho2/v1/sing-box/other/clear-log.sh"
+echo -e "${GB}[ INFO ]${NC} ${YB}Download All Menu Done${NC}"
+sleep 2
+chmod +x add-vmess
+chmod +x del-vmess
+chmod +x extend-vmess
+chmod +x trialvmess
+chmod +x cek-vmess
+chmod +x add-vless
+chmod +x del-vless
+chmod +x extend-vless
+chmod +x trialvless
+chmod +x cek-vless
+chmod +x add-trojan
+chmod +x del-trojan
+chmod +x extend-trojan
+chmod +x trialtrojan
+chmod +x cek-trojan
+chmod +x add-ss
+chmod +x del-ss
+chmod +x extend-ss
+chmod +x trialss
+chmod +x cek-ss
+chmod +x add-ss2022
+chmod +x del-ss2022
+chmod +x extend-ss2022
+chmod +x trialss2022
+chmod +x cek-ss2022
+chmod +x add-socks
+chmod +x del-socks
+chmod +x extend-socks
+chmod +x trialsocks
+chmod +x cek-socks
+chmod +x add-sing-box
+chmod +x del-sing-box
+chmod +x extend-sing-box
+chmod +x trialsing-box
+chmod +x cek-sing-box
+chmod +x log-create
+chmod +x log-vmess
+chmod +x log-vless
+chmod +x log-trojan
+chmod +x log-ss
+chmod +x log-ss2022
+chmod +x log-socks
+chmod +x log-allsing-box
+chmod +x menu
+chmod +x vmess
+chmod +x vless
+chmod +x trojan
+chmod +x shadowsocks
+chmod +x shadowsocks2022
+chmod +x socks
+chmod +x allsing-box
+chmod +x xp
+chmod +x dns
+chmod +x certsing-box
+chmod +x sing-boxmod
+chmod +x sing-boxofficial
+chmod +x about
+chmod +x clear-log
+clear
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Update Done${NC}"
+sleep 3
+menu
