b5e6fc80cc8b1838eb294bbf1b2e85ee2ccb78a9
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-05-27 17:27:48 +0700

    Update info-cpu.sh

--- other/info-cpu.sh
+++ other/info-cpu.sh
@@ -1,4 +1,4 @@
-domain=$(cat /etc/sing-box/domain)
+domain=$(cat /usr/local/etc/xray/domain)
 CHATID=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 3)
 CHATID="$CHATID"
 KEY="6860001875:AAEezcndU20DatRjLEhWnUYT9mP2ZVx9dkQ"
