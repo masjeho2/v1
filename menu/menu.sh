cec1f068ff7409215fadc4f74cfb231e409b7b39
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-06-08 07:12:34 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -65,7 +65,7 @@ echo -e " ${MB}[7]${NC} ${YB}Log Create Account${NC}    ${MB}[10]${NC} ${YB}Abou
 echo -e " ${MB}[8]${NC} ${YB}Speedtest${NC}             ${MB}[11]${NC} ${YB}Update Menu${NC}"
 echo -e " ${MB}[9]${NC} ${YB}Change Domain${NC}         ${MB}[12]${NC} ${YB}Update Core${NC}"
 echo -e " ${MB}[x]${NC} ${YB}Exit${NC}                  ${MB}[13]${NC} ${YB}Add Bot notif${NC}"
-echo -e "                                               ${MB}[14]${NC} ${YB}bekup${NC}"
+echo -e " ${MB}[14]${NC} ${YB}bekup${NC}"
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10 
 echo -e ""
 echo -e " ${WB}Press [ ctrl + c ] or Input x To Exit Script${NC}"
